b81e58f20fe567a9b669a6f80119d2ca056ff378
meruff <mathew.ruff@gmail.com>
2022-07-05 13:41:15 -0700

    finish tutorial

--- script.ts
+++ script.ts
@@ -1,8 +1,23 @@
 import { PrismaClient } from "@prisma/client";
+import { connect } from "http2";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.deleteMany();
+  await prisma.user.create({
+    data: {
+      name: "Tony",
+      age: 46,
+      email: "tony@nomail.com",
+    },
+  });
+
+  const user = await prisma.user.delete({
+    where: {
+      email: "sally@test3.com",
+    },
+  });
+
+  console.log(user);
 }
 
 main()
